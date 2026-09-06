f544719969c8b175ee8bad395f07a7f9c6229b5b
markhitchk <mark.hitchk@gmail.com>
2026-09-05 18:26:11 -0700

    Release 1.1.1 with dedicated Period Tracker UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pastelcal.app.final"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1010000
-        versionName = "1.1.0"
+        versionCode = 1010001
+        versionName = "1.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
